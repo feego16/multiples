a7906413227fd4dfe7457904ae391f1b14a9c574
orlando grant <feego@afeegoOrlando-MacBook.local>
2016-07-12 00:59:25 -0700

    just added these

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func isEven(num:Int) -> Bool{
+    return num % 2 == 0
+        
+}
+isEven(6)
